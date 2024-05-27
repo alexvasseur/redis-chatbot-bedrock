55e7b55130f97bd620b6948f6e92cdd34391f590
Alexandre Vasseur <avasseur@gmail.com>
2024-05-27 17:39:59 +0200

    import

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class AppConfig:
+    DOCS_FOLDER=os.environ["DOCS_FOLDER"]
+    REDIS_URL=os.environ["REDIS_URL"]
+    
+    CHUNK_SIZE=int(os.getenv("CHUNK_SIZE", 500))
+    CHUNK_OVERLAP=int(os.getenv("CHUNK_OVERLAP", 10))
+    PAGE_TITLE=os.getenv("PAGE_TITLE", "📃 Chat Your PDF")
+    PAGE_ICON=os.getenv("PAGE_ICON", "📃")
+    RETRIEVE_TOP_K=int(os.getenv("RETRIEVE_TOP_K", 5))
+    LLMCACHE_THRESHOLD=float(os.getenv("LLMCACHE_THRESHOLD", 0.75))
